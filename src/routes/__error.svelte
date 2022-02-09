06f44511c51453a60fedeb0342dd815a3a699b57
alanssitis <alanchunggt@gmail.com>
2022-02-09 17:38:11 -0500

    feat: added more to skeleton

---
+++ src/routes/__error.svelte
@@ -0,0 +1,1 @@
+<p>You are not supposed to be here!!</p>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,23 @@
   import "../app.css";
 </script>
 
+<!-- Navbar -->
+<div>
+    <p>navbar</p>
+    <a href="/">Rekor Monitor</a>
+    <a href="/people">People</a>
+    <a href="/documentation">API Documentation</a>
+    <a href="https://github.com/alanssitis/rekor-monitor">Source</a>
+</div>
+
+<!-- Content -->
 <slot />
+
+<!-- Footer -->
+<div>
+    <p>footer</p>
+    <a href="/">Rekor Monitor</a>
+    <a href="/people">People</a>
+    <a href="/documentation">API Documentation</a>
+    <a href="https://github.com/alanssitis/rekor-monitor">Source</a>
+</div>
