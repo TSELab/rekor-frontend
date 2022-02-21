2bf01bf4dcca54de2eb0dc9f5754d1283e85e900
alanssitis <alanchunggt@gmail.com>
2022-02-21 01:28:06 -0500

    feat: implemented what was remembered from the wireframe

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,17 +1,29 @@
 <script>
 	import '../app.css';
-	let showheader = false;
+	let hidden = false;
 </script>
 
 <!-- Navbar -->
-<nav class="flex items-center justify-between flex-wrap bg-purdue-gold p-6">
-	<div class="flex items-center flex-shrink-0 text-black mr-6">
-		<img id="logo" alt="TSE Lab Logo" src="tse-lab-logo.png" class="bg-white rounded-md mr-2 p-1" />
-		<span class="font-semibold text-xl tracking-tight">TSE Labs</span>
+<nav class="flex items-center justify-between flex-wrap bg-purdue-gold px-8 md:px-16 py-4">
+	<div class="flex items-center flex-shrink-0 mr-6">
+		<a href="https://lab.seal.purdue.wtf/">
+			<img
+				id="logo"
+				alt="TSE Lab Logo"
+				src="tse-logo-full.png"
+				class="hidden md:block max-h-16 object-cover mr-2 p-0.5"
+			/>
+			<img
+				id="logo"
+				alt="TSE Lab Logo"
+				src="tse-logo-part.png"
+				class="md:hidden max-h-16 object-cover mr-2 p-0.5"
+			/>
+		</a>
 	</div>
 	<div class="block lg:hidden">
 		<button
-			on:click={() => (showheader = !showheader)}
+			on:click={() => (hidden = !hidden)}
 			class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
 		>
 			<svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
@@ -19,14 +31,10 @@
 			>
 		</button>
 	</div>
-	<div
-		class="w-full block flex-grow text-white lg:flex lg:items-center lg:w-auto {showheader
-			? 'hidden'
-			: ''}"
-	>
-		<div class="text-sm lg:flex-grow">
+	<div class="w-full block flex-grow text-black text-md lg:flex lg:items-center lg:w-auto" class:hidden>
+		<div class="lg:flex-grow">
 			<a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-aged mr-6">
-				Home
+				Monitor
 			</a>
 			<a
 				href="/about"
@@ -39,9 +47,6 @@
 				class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-aged mr-6"
 			>
 				People
-			</a>
-			<a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-aged mr-6">
-				Rekor Monitor
 			</a>
 		</div>
 		<div>
@@ -56,24 +61,34 @@
 </nav>
 
 <!-- Content -->
-<div class="h-full m-6 flex-2">
+<div class="flex-auto">
 	<slot />
 </div>
 
 <!-- Footer -->
-<div class="bg-aged text-white p-6">
-	<ul>
-		<li>
-			<a href="/">Home</a>
-		</li>
-		<li>
-			<a href="/about">About</a>
-		</li>
-		<li>
-			<a href="/people">People</a>
-		</li>
-		<li>
-			<a href="/">Rekor Monitor</a>
-		</li>
-	</ul>
-</div>
+<footer class="shrink-0 m-0">
+    <div class="bg-rush text-black px-16 lg:px-28 pt-10 pb-10">
+        <h2 class="text-3xl font-bold">Partners</h2>
+    </div>
+    <div class="bg-steel text-white text-lg px-16 lg:px-24 pt-10 pb-12">
+        <ul>
+            <li class="my-3">
+                <a href="/" class="hover:text-dust">Monitor</a>
+            </li>
+            <li class="my-3">
+                <a href="/about" class="hover:text-dust">About</a>
+            </li>
+            <li class="my-3">
+                <a href="/people" class="hover:text-dust">People</a>
+            </li>
+            <li class="my-3">
+                <a
+                    href="https://github.com/alanssitis/rekor-monitor"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="hover:text-dust">Source</a
+                >
+            </li>
+        </ul>
+    </div>
+</footer>
